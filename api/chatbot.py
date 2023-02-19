bd4db118af38ef4d450528ba936712869baadacb
Marcel Samyn <marcel.samyn@lab900.com>
2023-02-19 11:03:37 +0100

    :bug: fix text splitting so no web data gets lost

--- api/chatbot.py
+++ api/chatbot.py
@@ -28,6 +28,44 @@ def add_website_content(result):
     return result
 
 
+def split_content(content, split_length):
+    segments = []
+    for line in content.split('\n'):
+        segments.extend(split_line(line, split_length))
+    segments = merge_segments(segments, split_length)
+    pprint(len(segments))
+    return segments
+
+
+def split_line(line, split_length):
+    words = line.split()
+    segments = []
+    current_segment = ""
+    for i, word in enumerate(words):
+        if len(current_segment + " " + word) <= split_length:
+            current_segment += (" " if current_segment else "") + word
+        else:
+            segments.append(current_segment)
+            current_segment = word
+    if current_segment:
+        segments.append(current_segment)
+    return segments
+
+
+def merge_segments(segments, split_length):
+    new_segments = []
+    current_segment = ""
+    for segment in segments:
+        if len(current_segment + " " + segment) > split_length and current_segment:
+            new_segments.append(current_segment.strip())
+            current_segment = segment
+        else:
+            current_segment += (" " if current_segment else "") + segment
+    if current_segment:
+        new_segments.append(current_segment.strip())
+    return new_segments
+
+
 def answer_question(question):
     results = search(question)
     results_with_content = [add_website_content(
@@ -37,13 +75,13 @@ def answer_question(question):
         {'name': result['name'],
          'url': result['url'],
          'snippet': snippet} for result in results_with_content
-        for snippet in result['content'].split('\n') if len(snippet) > 200]
+        for snippet in split_content(result['content'], 300)
+    ]
 
     documents = [gpt_index.Document(result['snippet'], extra_info={
                                     'url': result['url']}) for result in results_with_split_content]
 
-    index = gpt_index.GPTListIndex(documents)
+    index = gpt_index.GPTSimpleVectorIndex(documents)
 
     response = index.query(question, mode="embedding")
-    pprint(response)
     return response.response.strip('\n').strip()
